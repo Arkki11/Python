39c3f863b387dc40ffa0ff447dc911a05256fb83
Arkki <112556929+Arkki11@users.noreply.github.com>
2024-05-07 22:31:20 +0300

    Add files via upload

---
+++ largest_num.py
@@ -0,0 +1,8 @@
+largest_num = 0
+print("The current largest number", largest_num)
+for current_num in [9, 41, 12, 3, 74, 15] :
+    if current_num > largest_num :
+        largest_num = current_num
+    print(largest_num, current_num)
+
+print("The final largest number", largest_num)
